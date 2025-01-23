612b23c8a624dae51c7a623a878790655044cb71
assaf Bamberger <50671687+assafBam@users.noreply.github.com>
2025-01-23 16:08:14 +0200

    added whir

---
+++ whir/src/bin/main.rs
@@ -0,0 +1,418 @@
+use std::time::{Duration, Instant};
+
+use ark_crypto_primitives::{
+    crh::{CRHScheme, TwoToOneCRHScheme},
+    merkle_tree::Config,
+};
+use ark_ff::FftField;
+use ark_serialize::CanonicalSerialize;
+use nimue::{DefaultHash, IOPattern};
+use whir::{
+    cmdline_utils::{AvailableFields, AvailableMerkle, WhirType},
+    crypto::{
+        fields,
+        merkle_tree::{self, HashCounter},
+    },
+    parameters::*,
+    poly_utils::{coeffs::CoefficientList, MultilinearPoint},
+};
+
+use nimue::plugins::pow::blake3::Blake3PoW;
+
+use clap::Parser;
+
+use ark_crypto_primitives::merkle_tree::set_number_of_layers_single_trhead;
+use ark_crypto_primitives::merkle_tree::time::mt_get_time;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 't', long = "type", default_value = "PCS")]
+    protocol_type: WhirType,
+
+    #[arg(short = 'l', long, default_value = "100")]
+    security_level: usize,
+
+    #[arg(short = 'p', long)]
+    pow_bits: Option<usize>,
+
+    #[arg(short = 'd', long, default_value = "20")]
+    num_variables: usize,
+
+    #[arg(short = 'e', long = "evaluations", default_value = "1")]
+    num_evaluations: usize,
+
+    #[arg(short = 'r', long, default_value = "1")]
+    rate: usize,
+
+    #[arg(long = "reps", default_value = "1000")]
+    verifier_repetitions: usize,
+
+    #[arg(short = 'k', long = "fold", default_value = "4")]
+    folding_factor: usize,
+
+    #[arg(long = "sec", default_value = "ConjectureList")]
+    soundness_type: SoundnessType,
+
+    #[arg(long = "fold_type", default_value = "ProverHelps")]
+    fold_optimisation: FoldType,
+
+    #[arg(short = 'f', long = "field", default_value = "Goldilocks2")]
+    field: AvailableFields,
+
+    #[arg(long = "hash", default_value = "Blake3")]
+    merkle_tree: AvailableMerkle,
+
+    #[arg(short = 'y', long = "single_thread_layers", default_value = "10")]
+    single_thread_layers: usize,
+}
+
+type PowStrategy = Blake3PoW;
+
+fn main() {
+    let mut args = Args::parse();
+    let field = args.field;
+    let merkle = args.merkle_tree;
+    set_number_of_layers_single_trhead(args.single_thread_layers);
+
+    if args.pow_bits.is_none() {
+        args.pow_bits = Some(default_max_pow(args.num_variables, args.rate));
+    }
+
+    let mut rng = ark_std::test_rng();
+
+    match (field, merkle) {
+        (AvailableFields::Goldilocks1, AvailableMerkle::Blake3) => {
+            use fields::Field64 as F;
+            use merkle_tree::blake3 as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Goldilocks1, AvailableMerkle::Keccak256) => {
+            use fields::Field64 as F;
+            use merkle_tree::keccak as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Goldilocks2, AvailableMerkle::Blake3) => {
+            use fields::Field64_2 as F;
+            use merkle_tree::blake3 as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Goldilocks2, AvailableMerkle::Keccak256) => {
+            use fields::Field64_2 as F;
+            use merkle_tree::keccak as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Goldilocks3, AvailableMerkle::Blake3) => {
+            use fields::Field64_3 as F;
+            use merkle_tree::blake3 as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Goldilocks3, AvailableMerkle::Keccak256) => {
+            use fields::Field64_3 as F;
+            use merkle_tree::keccak as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Field128, AvailableMerkle::Blake3) => {
+            use fields::Field128 as F;
+            use merkle_tree::blake3 as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Field128, AvailableMerkle::Keccak256) => {
+            use fields::Field128 as F;
+            use merkle_tree::keccak as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Field192, AvailableMerkle::Blake3) => {
+            use fields::Field192 as F;
+            use merkle_tree::blake3 as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Field192, AvailableMerkle::Keccak256) => {
+            use fields::Field192 as F;
+            use merkle_tree::keccak as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Field256, AvailableMerkle::Blake3) => {
+            use fields::Field256 as F;
+            use merkle_tree::blake3 as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+
+        (AvailableFields::Field256, AvailableMerkle::Keccak256) => {
+            use fields::Field256 as F;
+            use merkle_tree::keccak as mt;
+
+            let (leaf_hash_params, two_to_one_params) = mt::default_config::<F>(&mut rng);
+            run_whir::<F, mt::MerkleTreeParams<F>>(args, leaf_hash_params, two_to_one_params);
+        }
+    }
+}
+
+fn run_whir<F, MerkleConfig>(
+    args: Args,
+    leaf_hash_params: <<MerkleConfig as Config>::LeafHash as CRHScheme>::Parameters,
+    two_to_one_params: <<MerkleConfig as Config>::TwoToOneHash as TwoToOneCRHScheme>::Parameters,
+) where
+    F: FftField + CanonicalSerialize,
+    MerkleConfig: Config<Leaf = [F]> + Clone,
+    MerkleConfig::InnerDigest: AsRef<[u8]> + From<[u8; 32]>,
+{
+    match args.protocol_type {
+        WhirType::PCS => run_whir_pcs::<F, MerkleConfig>(args, leaf_hash_params, two_to_one_params),
+        WhirType::LDT => {
+            run_whir_as_ldt::<F, MerkleConfig>(args, leaf_hash_params, two_to_one_params)
+        }
+    }
+}
+
+fn run_whir_as_ldt<F, MerkleConfig>(
+    args: Args,
+    leaf_hash_params: <<MerkleConfig as Config>::LeafHash as CRHScheme>::Parameters,
+    two_to_one_params: <<MerkleConfig as Config>::TwoToOneHash as TwoToOneCRHScheme>::Parameters,
+) where
+    F: FftField + CanonicalSerialize,
+    MerkleConfig: Config<Leaf = [F]> + Clone,
+    MerkleConfig::InnerDigest: AsRef<[u8]> + From<[u8; 32]>,
+{
+    use whir::whir_ldt::{
+        committer::Committer, iopattern::WhirIOPattern, parameters::WhirConfig, prover::Prover,
+        verifier::Verifier, whir_proof_size,
+    };
+
+    // Runs as a LDT
+    let security_level = args.security_level;
+    let pow_bits = args.pow_bits.unwrap();
+    let num_variables = args.num_variables;
+    let starting_rate = args.rate;
+    let reps = args.verifier_repetitions;
+    let folding_factor = args.folding_factor;
+    let fold_optimisation = args.fold_optimisation;
+    let soundness_type = args.soundness_type;
+
+    if args.num_evaluations > 1 {
+        println!("Warning: running as LDT but a number of evaluations to be proven was specified.");
+    }
+
+    let num_coeffs = 1 << num_variables;
+
+    let mv_params = MultivariateParameters::<F>::new(num_variables);
+
+    let whir_params = WhirParameters::<MerkleConfig, PowStrategy> {
+        security_level,
+        pow_bits,
+        folding_factor,
+        leaf_hash_params,
+        two_to_one_params,
+        soundness_type,
+        fold_optimisation,
+        _pow_parameters: Default::default(),
+        starting_log_inv_rate: starting_rate,
+    };
+
+    let params = WhirConfig::<F, MerkleConfig, PowStrategy>::new(mv_params, whir_params);
+
+    let io = IOPattern::<DefaultHash>::new("🌪️")
+        .commit_statement(&params)
+        .add_whir_proof(&params)
+        .clone();
+
+    let mut merlin = io.to_merlin();
+
+    println!("=========================================");
+    println!("Whir (LDT) 🌪️");
+    println!("Field: {:?} and MT: {:?}", args.field, args.merkle_tree);
+    println!("{}", params);
+    if !params.check_pow_bits() {
+        println!("WARN: more PoW bits required than what specified.");
+    }
+
+    use ark_ff::Field;
+    let polynomial = CoefficientList::new(
+        (0..num_coeffs)
+            .map(<F as Field>::BasePrimeField::from)
+            .collect(),
+    );
+
+    let whir_prover_time = Instant::now();
+
+    let committer = Committer::new(params.clone());
+    let witness = committer.commit(&mut merlin, polynomial).unwrap();
+
+    let prover = Prover(params.clone());
+
+    let proof = prover.prove(&mut merlin, witness).unwrap();
+
+    dbg!(whir_prover_time.elapsed());
+    dbg!(whir_proof_size(merlin.transcript(), &proof));
+
+    // Just not to count that initial inversion (which could be precomputed)
+    let verifier = Verifier::new(params);
+
+    HashCounter::reset();
+    let whir_verifier_time = Instant::now();
+    for _ in 0..reps {
+        let mut arthur = io.to_arthur(merlin.transcript());
+        verifier.verify(&mut arthur, &proof).unwrap();
+    }
+    dbg!(whir_verifier_time.elapsed() / reps as u32);
+    dbg!(HashCounter::get() as f64 / reps as f64);
+
+    println!(
+        "Merkle Tree calculation time: {:?}",
+        Duration::from_nanos(mt_get_time().unwrap_or(0))
+    );
+}
+
+fn run_whir_pcs<F, MerkleConfig>(
+    args: Args,
+    leaf_hash_params: <<MerkleConfig as Config>::LeafHash as CRHScheme>::Parameters,
+    two_to_one_params: <<MerkleConfig as Config>::TwoToOneHash as TwoToOneCRHScheme>::Parameters,
+) where
+    F: FftField + CanonicalSerialize,
+    MerkleConfig: Config<Leaf = [F]> + Clone,
+    MerkleConfig::InnerDigest: AsRef<[u8]> + From<[u8; 32]>,
+{
+    use whir::whir::{
+        committer::Committer, iopattern::WhirIOPattern, parameters::WhirConfig, prover::Prover,
+        verifier::Verifier, whir_proof_size, Statement,
+    };
+
+    // Runs as a PCS
+    let security_level = args.security_level;
+    let pow_bits = args.pow_bits.unwrap();
+    let num_variables = args.num_variables;
+    let starting_rate = args.rate;
+    let reps = args.verifier_repetitions;
+    let folding_factor = args.folding_factor;
+    let fold_optimisation = args.fold_optimisation;
+    let soundness_type = args.soundness_type;
+    let num_evaluations = args.num_evaluations;
+
+    if num_evaluations == 0 {
+        println!("Warning: running as PCS but no evaluations specified.");
+    }
+
+    let num_coeffs = 1 << num_variables;
+
+    let mv_params = MultivariateParameters::<F>::new(num_variables);
+
+    let whir_params = WhirParameters::<MerkleConfig, PowStrategy> {
+        security_level,
+        pow_bits,
+        folding_factor,
+        leaf_hash_params,
+        two_to_one_params,
+        soundness_type,
+        fold_optimisation,
+        _pow_parameters: Default::default(),
+        starting_log_inv_rate: starting_rate,
+    };
+
+    let params = WhirConfig::<F, MerkleConfig, PowStrategy>::new(mv_params, whir_params);
+
+    let io = IOPattern::<DefaultHash>::new("🌪️")
+        .commit_statement(&params)
+        .add_whir_proof(&params)
+        .clone();
+
+    let mut merlin = io.to_merlin();
+
+    println!("=========================================");
+    println!("Whir (PCS) 🌪️");
+    println!("Field: {:?} and MT: {:?}", args.field, args.merkle_tree);
+    println!("{}", params);
+    if !params.check_pow_bits() {
+        println!("WARN: more PoW bits required than what specified.");
+    }
+
+    use ark_ff::Field;
+    let polynomial = CoefficientList::new(
+        (0..num_coeffs)
+            .map(<F as Field>::BasePrimeField::from)
+            .collect(),
+    );
+    let points: Vec<_> = (0..num_evaluations)
+        .map(|i| MultilinearPoint(vec![F::from(i as u64); num_variables]))
+        .collect();
+    let evaluations = points
+        .iter()
+        .map(|point| polynomial.evaluate_at_extension(&point))
+        .collect();
+
+    let statement = Statement {
+        points,
+        evaluations,
+    };
+
+    let whir_prover_time = Instant::now();
+
+    let committer = Committer::new(params.clone());
+    let witness = committer.commit(&mut merlin, polynomial).unwrap();
+
+    let prover = Prover(params.clone());
+
+    let proof = prover
+        .prove(&mut merlin, statement.clone(), witness)
+        .unwrap();
+
+    println!("Prover time: {:.1?}", whir_prover_time.elapsed());
+    println!(
+        "Proof size: {:.1} KiB",
+        whir_proof_size(merlin.transcript(), &proof) as f64 / 1024.0
+    );
+
+    // Just not to count that initial inversion (which could be precomputed)
+    let verifier = Verifier::new(params);
+
+    HashCounter::reset();
+    let whir_verifier_time = Instant::now();
+    for _ in 0..reps {
+        let mut arthur = io.to_arthur(merlin.transcript());
+        verifier.verify(&mut arthur, &statement, &proof).unwrap();
+    }
+    println!(
+        "Verifier time: {:.1?}",
+        whir_verifier_time.elapsed() / reps as u32
+    );
+    println!(
+        "Average hashes: {:.1}k",
+        (HashCounter::get() as f64 / reps as f64) / 1000.0
+    );
+
+    println!(
+        "Merkle Tree calculation time: {:?}",
+        Duration::from_nanos(mt_get_time().unwrap_or(0))
+    );
+}
---
+++ whir/src/whir/prover.rs
@@ -0,0 +1,335 @@
+use super::{committer::Witness, parameters::WhirConfig, Statement, WhirProof};
+use crate::{
+    domain::Domain,
+    ntt::expand_from_coeff,
+    parameters::FoldType,
+    poly_utils::{
+        coeffs::CoefficientList,
+        fold::{compute_fold, restructure_evaluations},
+        MultilinearPoint,
+    },
+    sumcheck::prover_not_skipping::SumcheckProverNotSkipping,
+    utils::{self, expand_randomness},
+};
+use ark_crypto_primitives::merkle_tree::{Config, MerkleTree, MultiPath};
+use ark_ff::FftField;
+use ark_poly::EvaluationDomain;
+use nimue::{
+    plugins::{
+        ark::{FieldChallenges, FieldWriter},
+        pow::{self, PoWChallenge},
+    },
+    ByteChallenges, ByteWriter, Merlin, ProofResult,
+};
+use rand::{Rng, SeedableRng};
+
+#[cfg(feature = "parallel")]
+use rayon::prelude::*;
+
+pub struct Prover<F, MerkleConfig, PowStrategy>(pub WhirConfig<F, MerkleConfig, PowStrategy>)
+where
+    F: FftField,
+    MerkleConfig: Config;
+
+impl<F, MerkleConfig, PowStrategy> Prover<F, MerkleConfig, PowStrategy>
+where
+    F: FftField,
+    MerkleConfig: Config<Leaf = [F]>,
+    MerkleConfig::InnerDigest: AsRef<[u8]>,
+    PowStrategy: pow::PowStrategy,
+{
+    fn validate_parameters(&self) -> bool {
+        self.0.mv_parameters.num_variables
+            == (self.0.n_rounds() + 1) * self.0.folding_factor + self.0.final_sumcheck_rounds
+    }
+
+    fn validate_statement(&self, statement: &Statement<F>) -> bool {
+        statement
+            .points
+            .iter()
+            .all(|point| point.0.len() == self.0.mv_parameters.num_variables)
+    }
+
+    fn validate_witness(&self, witness: &Witness<F, MerkleConfig>) -> bool {
+        witness.polynomial.num_variables() == self.0.mv_parameters.num_variables
+    }
+
+    pub fn prove(
+        &self,
+        merlin: &mut Merlin,
+        statement: Statement<F>,
+        witness: Witness<F, MerkleConfig>,
+    ) -> ProofResult<WhirProof<MerkleConfig, F>>
+    where
+        Merlin: FieldChallenges<F> + ByteWriter,
+    {
+        assert!(self.validate_parameters());
+        assert!(self.validate_statement(&statement));
+        assert!(self.validate_witness(&witness));
+
+        let [combination_randomness_gen] = merlin.challenge_scalars()?;
+        let initial_claims: Vec<_> = witness
+            .ood_points
+            .into_iter()
+            .map(|ood_point| {
+                MultilinearPoint::expand_from_univariate(
+                    ood_point,
+                    self.0.mv_parameters.num_variables,
+                )
+            })
+            .chain(statement.points)
+            .collect();
+        let combination_randomness =
+            expand_randomness(combination_randomness_gen, initial_claims.len());
+        let initial_answers: Vec<_> = witness
+            .ood_answers
+            .into_iter()
+            .chain(statement.evaluations)
+            .collect();
+
+        let mut sumcheck_prover = SumcheckProverNotSkipping::new(
+            witness.polynomial.clone(),
+            &initial_claims,
+            &combination_randomness,
+            &initial_answers,
+        );
+
+        let folding_randomness = sumcheck_prover.compute_sumcheck_polynomials::<PowStrategy>(
+            merlin,
+            self.0.folding_factor,
+            self.0.starting_folding_pow_bits,
+        )?;
+
+        let round_state = RoundState {
+            domain: self.0.starting_domain.clone(),
+            round: 0,
+            sumcheck_prover,
+            folding_randomness,
+            coefficients: witness.polynomial,
+            prev_merkle: witness.merkle_tree,
+            prev_merkle_answers: witness.merkle_leaves,
+            merkle_proofs: vec![],
+        };
+
+        self.round(merlin, round_state)
+    }
+
+    fn round(
+        &self,
+        merlin: &mut Merlin,
+        mut round_state: RoundState<F, MerkleConfig>,
+    ) -> ProofResult<WhirProof<MerkleConfig, F>> {
+        // Fold the coefficients
+        let folded_coefficients = round_state
+            .coefficients
+            .fold(&round_state.folding_randomness);
+
+        let num_variables =
+            self.0.mv_parameters.num_variables - (round_state.round + 1) * self.0.folding_factor;
+
+        // Base case
+        if round_state.round == self.0.n_rounds() {
+            // Coefficients of the polynomial
+            merlin.add_scalars(folded_coefficients.coeffs())?;
+
+            // Final verifier queries and answers
+            let mut queries_seed = [0u8; 32];
+            merlin.fill_challenge_bytes(&mut queries_seed)?;
+            let mut final_gen = rand_chacha::ChaCha20Rng::from_seed(queries_seed);
+            let final_challenge_indexes = utils::dedup((0..self.0.final_queries).map(|_| {
+                final_gen.gen_range(0..round_state.domain.folded_size(self.0.folding_factor))
+            }));
+
+            let merkle_proof = round_state
+                .prev_merkle
+                .generate_multi_proof(final_challenge_indexes.clone())
+                .unwrap();
+            let fold_size = 1 << self.0.folding_factor;
+            let answers = final_challenge_indexes
+                .into_iter()
+                .map(|i| {
+                    round_state.prev_merkle_answers[i * fold_size..(i + 1) * fold_size].to_vec()
+                })
+                .collect();
+            round_state.merkle_proofs.push((merkle_proof, answers));
+
+            // PoW
+            if self.0.final_pow_bits > 0. {
+                merlin.challenge_pow::<PowStrategy>(self.0.final_pow_bits)?;
+            }
+
+            // Final sumcheck
+            round_state
+                .sumcheck_prover
+                .compute_sumcheck_polynomials::<PowStrategy>(
+                    merlin,
+                    self.0.final_sumcheck_rounds,
+                    self.0.final_folding_pow_bits,
+                )?;
+
+            return Ok(WhirProof(round_state.merkle_proofs));
+        }
+
+        let round_params = &self.0.round_parameters[round_state.round];
+
+        // Fold the coefficients, and compute fft of polynomial (and commit)
+        let new_domain = round_state.domain.scale(2);
+        let expansion = new_domain.size() / folded_coefficients.num_coeffs();
+        let evals = expand_from_coeff(folded_coefficients.coeffs(), expansion);
+        // TODO: `stack_evaluations` and `restructure_evaluations` are really in-place algorithms.
+        // They also partially overlap and undo one another. We should merge them.
+        let folded_evals = utils::stack_evaluations(evals, self.0.folding_factor);
+        let folded_evals = restructure_evaluations(
+            folded_evals,
+            self.0.fold_optimisation,
+            new_domain.backing_domain.group_gen(),
+            new_domain.backing_domain.group_gen_inv(),
+            self.0.folding_factor,
+        );
+
+        #[cfg(not(feature = "parallel"))]
+        let leafs_iter = folded_evals.chunks_exact(1 << self.0.folding_factor);
+        #[cfg(feature = "parallel")]
+        let leafs_iter = folded_evals.par_chunks_exact(1 << self.0.folding_factor);
+        let merkle_tree = MerkleTree::<MerkleConfig>::new(
+            &self.0.leaf_hash_params,
+            &self.0.two_to_one_params,
+            leafs_iter,
+        )
+        .unwrap();
+
+        let root = merkle_tree.root();
+        merlin.add_bytes(root.as_ref())?;
+
+        // OOD Samples
+        let mut ood_points = vec![F::ZERO; round_params.ood_samples];
+        let mut ood_answers = Vec::with_capacity(round_params.ood_samples);
+        if round_params.ood_samples > 0 {
+            merlin.fill_challenge_scalars(&mut ood_points)?;
+            ood_answers.extend(ood_points.iter().map(|ood_point| {
+                folded_coefficients.evaluate(&MultilinearPoint::expand_from_univariate(
+                    *ood_point,
+                    num_variables,
+                ))
+            }));
+            merlin.add_scalars(&ood_answers)?;
+        }
+
+        // STIR queries
+        let mut stir_queries_seed = [0u8; 32];
+        merlin.fill_challenge_bytes(&mut stir_queries_seed)?;
+        let mut stir_gen = rand_chacha::ChaCha20Rng::from_seed(stir_queries_seed);
+        let stir_challenges_indexes =
+            utils::dedup((0..round_params.num_queries).map(|_| {
+                stir_gen.gen_range(0..round_state.domain.folded_size(self.0.folding_factor))
+            }));
+        let domain_scaled_gen = round_state
+            .domain
+            .backing_domain
+            .element(1 << self.0.folding_factor);
+        let stir_challenges: Vec<_> = ood_points
+            .into_iter()
+            .chain(
+                stir_challenges_indexes
+                    .iter()
+                    .map(|i| domain_scaled_gen.pow([*i as u64])),
+            )
+            .map(|univariate| MultilinearPoint::expand_from_univariate(univariate, num_variables))
+            .collect();
+
+        let merkle_proof = round_state
+            .prev_merkle
+            .generate_multi_proof(stir_challenges_indexes.clone())
+            .unwrap();
+        let fold_size = 1 << self.0.folding_factor;
+        let answers: Vec<_> = stir_challenges_indexes
+            .iter()
+            .map(|i| round_state.prev_merkle_answers[i * fold_size..(i + 1) * fold_size].to_vec())
+            .collect();
+        // Evaluate answers in the folding randomness.
+        let mut stir_evaluations = ood_answers.clone();
+        match self.0.fold_optimisation {
+            FoldType::Naive => {
+                // See `Verifier::compute_folds_full`
+                let domain_size = round_state.domain.backing_domain.size();
+                let domain_gen = round_state.domain.backing_domain.element(1);
+                let domain_gen_inv = domain_gen.inverse().unwrap();
+                let coset_domain_size = 1 << self.0.folding_factor;
+                let coset_generator_inv =
+                    domain_gen_inv.pow([(domain_size / coset_domain_size) as u64]);
+                stir_evaluations.extend(stir_challenges_indexes.iter().zip(&answers).map(
+                    |(index, answers)| {
+                        // The coset is w^index * <w_coset_generator>
+                        //let _coset_offset = domain_gen.pow(&[*index as u64]);
+                        let coset_offset_inv = domain_gen_inv.pow([*index as u64]);
+
+                        compute_fold(
+                            answers,
+                            &round_state.folding_randomness.0,
+                            coset_offset_inv,
+                            coset_generator_inv,
+                            F::from(2).inverse().unwrap(),
+                            self.0.folding_factor,
+                        )
+                    },
+                ))
+            }
+            FoldType::ProverHelps => stir_evaluations.extend(answers.iter().map(|answers| {
+                CoefficientList::new(answers.to_vec()).evaluate(&round_state.folding_randomness)
+            })),
+        }
+        round_state.merkle_proofs.push((merkle_proof, answers));
+
+        // PoW
+        if round_params.pow_bits > 0. {
+            merlin.challenge_pow::<PowStrategy>(round_params.pow_bits)?;
+        }
+
+        // Randomness for combination
+        let [combination_randomness_gen] = merlin.challenge_scalars()?;
+        let combination_randomness =
+            expand_randomness(combination_randomness_gen, stir_challenges.len());
+
+        round_state.sumcheck_prover.add_new_equality(
+            &stir_challenges,
+            &combination_randomness,
+            &stir_evaluations,
+        );
+
+        let folding_randomness = round_state
+            .sumcheck_prover
+            .compute_sumcheck_polynomials::<PowStrategy>(
+                merlin,
+                self.0.folding_factor,
+                round_params.folding_pow_bits,
+            )?;
+
+        let round_state = RoundState {
+            round: round_state.round + 1,
+            domain: new_domain,
+            sumcheck_prover: round_state.sumcheck_prover,
+            folding_randomness,
+            coefficients: folded_coefficients, // TODO: Is this redundant with `sumcheck_prover.coeff` ?
+            prev_merkle: merkle_tree,
+            prev_merkle_answers: folded_evals,
+            merkle_proofs: round_state.merkle_proofs,
+        };
+
+        self.round(merlin, round_state)
+    }
+}
+
+struct RoundState<F, MerkleConfig>
+where
+    F: FftField,
+    MerkleConfig: Config,
+{
+    round: usize,
+    domain: Domain<F>,
+    sumcheck_prover: SumcheckProverNotSkipping<F>,
+    folding_randomness: MultilinearPoint<F>,
+    coefficients: CoefficientList<F>,
+    prev_merkle: MerkleTree<MerkleConfig>,
+    prev_merkle_answers: Vec<F>,
+    merkle_proofs: Vec<(MultiPath<MerkleConfig>, Vec<Vec<F>>)>,
+}
